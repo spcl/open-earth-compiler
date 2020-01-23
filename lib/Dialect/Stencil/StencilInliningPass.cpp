b1a26621b798dfd12497be2f08f1a14ce9a68e10
Tobias Gysi <tobias.gysi@inf.ethz.ch>
2020-01-23 17:18:01 +0100

    second bugfix

--- lib/Dialect/Stencil/StencilInliningPass.cpp
+++ lib/Dialect/Stencil/StencilInliningPass.cpp
@@ -113,7 +113,7 @@ struct RerouteRewrite : public OpRewritePattern<stencil::ApplyOp> {
     // Search consumer connected to a single producer
     SmallVector<Operation *, 10> producerOps;
     for (auto operand : applyOp.operands()) {
-      if (isa<stencil::ApplyOp>(operand.getDefiningOp())) {
+      if (isa_and_nonnull<stencil::ApplyOp>(operand.getDefiningOp())) {
         if (!llvm::is_contained(producerOps, operand.getDefiningOp()))
           producerOps.push_back(operand.getDefiningOp());
       }
@@ -238,7 +238,7 @@ struct InliningRewrite : public OpRewritePattern<stencil::ApplyOp> {
                                      PatternRewriter &rewriter) const override {
     // Search producer apply op
     for (auto operand : applyOp.operands()) {
-      if (isa<stencil::ApplyOp>(operand.getDefiningOp())) {
+      if (isa_and_nonnull<stencil::ApplyOp>(operand.getDefiningOp())) {
         // Check if multiple consumers
         auto producerResults = operand.getDefiningOp()->getResults();
         for (auto result : producerResults) {
