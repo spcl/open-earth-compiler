01cf0db0ccc67ef1693952e66a324a8d996f491b
Tobias Gysi <tobias.gysi@inf.ethz.ch>
2020-05-13 12:20:33 +0200

    assert and load prototype lowering

--- include/Conversion/StencilToStandard/ConvertStencilToStandard.h
+++ include/Conversion/StencilToStandard/ConvertStencilToStandard.h
@@ -2,12 +2,52 @@
 #define CONVERSION_STENCILTOSTANDARD_CONVERTSTENCILTOSTANDARD_H
 
 #include "Dialect/Stencil/StencilOps.h"
+#include "mlir/IR/MLIRContext.h"
 #include "mlir/Transforms/DialectConversion.h"
 
 namespace mlir {
+namespace stencil {
 
-void populateStencilToStandardConversionPatterns(
-    OwningRewritePatternList &patterns, MLIRContext *ctx);
-}
+/// Convert stencil types to standard types
+struct StencilTypeConverter : public TypeConverter {
+  using TypeConverter::TypeConverter;
+
+  /// Create a stencil type converter using the default conversions
+  StencilTypeConverter();
+
+  /// Convert stencil field types to memref types
+  Type convertFieldType(FieldType type);
+};
+
+/// Base class for the stencil to standard operation conversions
+class StencilToStdPattern : public ConversionPattern {
+public:
+  StencilToStdPattern(StringRef rootOpName, MLIRContext *context,
+                           StencilTypeConverter &typeConverter,
+                           PatternBenefit benefit = 1);
+
+protected:
+  /// Reference to the type converter
+  StencilTypeConverter &typeConverter;
+};
+
+/// Helper class to implement patterns that match one source operation
+template <typename OpTy>
+class StencilOpToStdPattern : public StencilToStdPattern {
+public:
+  StencilOpToStdPattern(MLIRContext *context,
+                             StencilTypeConverter &typeConverter,
+                             PatternBenefit benefit = 1)
+      : StencilToStdPattern(OpTy::getOperationName(), context,
+                                 typeConverter, benefit) {}
+};
+
+/// Helper method to populate the conversion pattern list
+void populateStencilToStdConversionPatterns(
+    MLIRContext *ctx, StencilTypeConverter &typeConveter,
+    OwningRewritePatternList &patterns);
+
+} // namespace stencil
+} // namespace mlir
 
 #endif // CONVERSION_STENCILTOSTANDARD_CONVERTSTENCILTOSTANDARD_H
